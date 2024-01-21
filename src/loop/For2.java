e4e60b353944559a6a0de9899d0f70067652151a
vanhelsing <vanhelsing@naver.com>
2024-01-21 17:47:11 +0900

    세번째 학습 "Java 소스"

--- src/loop/For2.java
+++ src/loop/For2.java
@@ -3,7 +3,7 @@ package loop;
 public class For2 {
     public static void main(String[] args) {
         int sum = 0;
-        int endNum = 4;
+        int endNum = 3;
 
         for (int i = 1; i <= endNum; i++) {
             sum = sum + i;
--- src/loop/While2_3.java
+++ src/loop/While2_3.java
@@ -4,17 +4,12 @@ public class While2_3 {
     public static void main(String[] args) {
         int sum = 0;
         int i  = 1;
+        int endNum = 3;
 
-        sum = sum + 1;
-        System.out.println("i = "+ 1 + " sum = "  + sum);
-
-
-        sum = sum + 2;
-        System.out.println("i = "+ 2 + " sum = "  + sum);
-
-
-        sum = sum + 3;
-        System.out.println("i = "+ 3 + " sum = "  + sum);
-
+        while (i <= endNum) {
+            sum = sum + i;
+            System.out.println("i = " + i + "sum = " + sum);
+            i++;
+        }
     }
 }
