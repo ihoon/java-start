1b998f9001ba46e2f0092c171201fe2b5f567d77
vanhelsing <vanhelsing@naver.com>
2024-02-12 18:17:28 +0900

    메서드 반환값 첫번째

---
+++ src/method/MethodReturn2.java
@@ -0,0 +1,16 @@
+package method;
+
+public class MethodReturn2 {
+    public static void main(String[] args) {
+        checkAge(10);
+        checkAge(20);
+    }
+
+    public static void checkAge(int age) {
+        if (age < 18) {
+            System.out.println(age + "살, 미성년자는 입장이 불가능 합니다.");
+            return;
+        }
+        System.out.println(age + "살, 입장 가능합니다.");
+    }
+}
